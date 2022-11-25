1841b854f73f8cb552f3de8a90f71c8b89392475
Channu45 <111673372+Channu45@users.noreply.github.com>
2022-11-25 19:51:01 +0530

    Assignment Submitted
    DAY-3 Assignment Submitted

---
+++ DAY-3.jsx
@@ -0,0 +1,16 @@
+<article class="card">
+ <div class="card__media">...</div>
+
+ <div class="card__content">
+   <header class="card__header">
+     <h2 class="card__title">City Lights in New York</h2>
+     <div class="card__subtitle">The city that never sleeps</div>
+   </header>
+   <p class="card__excerpt">New York, the largest city in the U.S., is an architectural marvel with plenty of historic monuments, magnificent buildings and countless dazzling skyscrapers.</p>
+
+   <footer class="card__meta" role="contentinfo">
+     <span class="card__post-date">6 min ago</span>
+     <span class="card__comments">39 comments</span>
+   </footer>
+ </div>
+</article>
